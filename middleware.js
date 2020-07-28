ced6808abaf5762cbfe48d3aa4a98fa5c54db248
ludmila <ludmila.lyiu@gmail.com>
2020-07-28 01:09:21 -0300

    documentacion API

--- middleware.js
+++ middleware.js
@@ -1,20 +1,21 @@
 //checkear body content (title - completed)
 function checkBodyContent(req, res, next) {
-        let data = req.body;
-        let title = data.titulo;
-        let completed = data.completed;
+    let data = req.body;
+    let title = data.titulo;
+    let completed = data.completed;
 
-        if (!title) {
-            res.status(400).send("No se indico la propiedad 'titulo'");
-            return;
-        }
-        if (completed == undefined) {
-            res.status(400).send("No se indico la propiedad 'completed'");
-            return;
-        }
-
-        next();
+    if (!title) {
+        res.status(400).send("No se indico la propiedad 'titulo'");
+        return;
+    }
+    if (completed == undefined) {
+        res.status(400).send("No se indico la propiedad 'completed'");
+        return;
     }
 
+    next();
+}
+
+//agregar middleware para verificar que existe el ID del item solicitado
 
 module.exports = { checkBodyContent: checkBodyContent };
